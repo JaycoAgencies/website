9fb7fdd27ffbee9dd9010770433a2486fdeccfc6
nikoionixx <nikhil.r@ionixxtech.com>
2023-03-11 14:14:17 +0530

    Values card

--- client/src/components/cards/Value.card.tsx
+++ client/src/components/cards/Value.card.tsx
@@ -1,5 +1,37 @@
-const Value = () => {
-    return (<div>Values</div>);
+import { FC } from 'react';
+import Text from '../Text';
+import value from 'public/assets/images/value.png';
+import value2 from 'public/assets/images/value2.png';
+import Image from '@/components/Image';
+interface ValueCard {
+  title: string;
+  description: string;
+  imagePosition: 'left' | 'right';
+}
+
+const ValueCard: FC<ValueCard> = ({ title, description, imagePosition }) => {
+  return (
+    <div
+      className={`flex gap-10 items-center ${
+        imagePosition === 'left' ? 'flex-row-reverse' : 'flex-row'
+      }`}
+    >
+      <div className="flex flex-col gap-4">
+        <Text
+          text={title}
+          className={'text-white font-medium text-3xl pb-2'}
+        ></Text>
+        <div className="h-[0.5px] bg-[#FCF9F9]"></div>
+        <Text
+          text={description}
+          className={'text-white font-light text-lg pb-2'}
+        ></Text>
+      </div>
+      <div className="flex">
+        <Image src={value} alt={'value'} width={293} height={386}></Image>
+      </div>
+    </div>
+  );
 };
 
-export default Value;
+export default ValueCard;
--- client/src/modules/home/VauesWeProvide.tsx
+++ client/src/modules/home/VauesWeProvide.tsx
@@ -1,5 +1,8 @@
+import ValueCard from '@/components/cards/Value.card';
+
 import SectionLayout from '@/components/styling/sectionLayout';
 import Text from '@/components/Text';
+
 
 const ValuesWeProvide = () => {
   return (
@@ -9,6 +12,35 @@ const ValuesWeProvide = () => {
           text={'Values we provide.'}
           className="dark:textDark text-white text-[64px] uppercase"
         ></Text>
+      </div>
+
+      <div className="flex mt-10 gap-20 justify-end items-center">
+        <div className="flex flex-col justify-between items-center">
+          <ValueCard
+            title="Value"
+            description="Lorem Ipsum has been the industry's standard dummy"
+            imagePosition={'right'}
+          ></ValueCard>
+
+          <ValueCard
+            title="Value"
+            description="Lorem Ipsum has been the industry's standard dummy"
+            imagePosition={'right'}
+          ></ValueCard>
+        </div>
+        <div className="flex flex-col justify-between">
+          <ValueCard
+            title="Value"
+            description="Lorem Ipsum has been the industry's standard dummy"
+            imagePosition={'left'}
+          ></ValueCard>
+
+          <ValueCard
+            title="Value"
+            description="Lorem Ipsum has been the industry's standard dummy"
+            imagePosition={'left'}
+          ></ValueCard>
+        </div>
       </div>
     </SectionLayout>
   );
